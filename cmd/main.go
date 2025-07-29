db012b20802a90d6513d1df481ea6c14bc90579c
heitormbonfim <heitor.mb27@gmail.com>
2025-07-29 17:48:36 -0300

    feat: executing tasks table creation

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 
 	"github.com/heitormbonfim/go-native-api/config"
+	"github.com/heitormbonfim/go-native-api/models"
 )
 
 func main() {
 	dbConnection := config.SetupDB()
 	defer dbConnection.Close()
 
-	err := http.ListenAndServe(":8080", nil)
+	_, err := dbConnection.Exec(models.CreateTableSQL)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 		return
